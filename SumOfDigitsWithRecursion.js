c61c10ba24cc32d098c32fed211fe98e8025414b
Kirti Jalan <kirtijalan194@gmail.com>
2018-06-24 20:37:27 +0530

    Update SumOfDigitsWithRecursion.js

--- SumOfDigitsWithRecursion.js
+++ SumOfDigitsWithRecursion.js
@@ -1,15 +1,19 @@
 function sumOfDigits(num){
 	let sum = 0; 
-  function addToSum(num){
-      if(num>0){
-        sum = sum + num%10;
-        addToSum(num/10);
+  if(num==1){
+  	return 1;
+  }else{
+  	function addToSum(num){
+      if(num>0){ 
+      	sum = sum + num%10;
+        addToSum(parseInt(num/10));
      } 
+  	}
+  	addToSum(num);
+  	return (Math.floor(sum));
   }
-  addToSum(num);
-  return Math.floor(sum);
 }
 
-let newNum = 123;
+let newNum = 12345;
 let result = sumOfDigits(newNum)
 document.write(result);
